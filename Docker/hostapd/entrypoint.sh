0db6aeeef47f0f8c7dcf67e8c3f1f65667c363a8
Arjan van Schoonhoven <arjan.vanschoonhoven@ultimatesoftware.nl>
2018-10-06 00:08:39 +0200

    hostapd changes

--- Docker/hostapd/entrypoint.sh
+++ Docker/hostapd/entrypoint.sh
@@ -13,15 +13,13 @@ term_handler() {
 ifconfig wlan0 10.0.0.1/24
 
 /etc/init.d/dbus start
-/etc/init.d/hostapd start
 /etc/init.d/dnsmasq start
+/etc/init.d/hostapd start
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE
-if [ ! $? -eq 0 ]
-then
-    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-fi
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
 
 # setup handlers
 trap term_handler SIGTERM
