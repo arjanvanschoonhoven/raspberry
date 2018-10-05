6bd0b3555aa3ac79147b9d88e29c2efb970c4f7b
Arjan van Schoonhoven <arjan.vanschoonhoven@ultimatesoftware.nl>
2018-10-05 23:06:59 +0200

    Changed entrypoint

--- Docker/hostapd/entrypoint.sh
+++ Docker/hostapd/entrypoint.sh
@@ -12,16 +12,6 @@ term_handler() {
 
 ifconfig wlan0 10.0.0.1/24
 
-if [ -z "$SSID" -a -z "$PASSWORD" ]; then
-  ssid="Pi3-AP"
-  password="raspberry"
-else
-  ssid=$SSID
-  password=$PASSWORD
-fi
-sed -i "s/ssid=.*/ssid=$ssid/g" /etc/hostapd/hostapd.conf
-sed -i "s/wpa_passphrase=.*/wpa_passphrase=$password/g" /etc/hostapd/hostapd.conf
-
 /etc/init.d/dbus start
 /etc/init.d/hostapd start
 /etc/init.d/dnsmasq start
