b405cc9047226c2fcff73a3e27b72fd2affbb54c
Arjan van Schoonhoven <arjan.vanschoonhoven@ultimatesoftware.nl>
2018-10-09 13:59:52 +0200

    More changes

--- Docker/hostapd/entrypoint.sh
+++ Docker/hostapd/entrypoint.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# SIGTERM-handler
-term_handler() {
-  echo "Get SIGTERM"
-  iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-  /etc/init.d/dnsmasq stop
-  /etc/init.d/hostapd stop
-  /etc/init.d/dbus stop
-  kill -TERM "$child" 2> /dev/null
-}
-
-ifconfig wlan0 10.0.0.1/24
-
-/etc/init.d/dbus start
-/etc/init.d/dnsmasq start
-
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
+
+/usr/sbin/
+
+/etc/init.d/dbus start
+/etc/init.d/dnsmasq start
 
 # setup handlers
 trap term_handler SIGTERM
--- Docker/hostapd2/entrypoint.sh
+++ Docker/hostapd2/entrypoint.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
-# SIGTERM-handler
-term_handler() {
-  echo "Get SIGTERM"
-  iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-  /etc/init.d/dnsmasq stop
-  /etc/init.d/hostapd stop
-  /etc/init.d/dbus stop
-  kill -TERM "$child" 2> /dev/null
-}
-
-ifconfig wlan0 10.0.0.1/24
-
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
-
-/etc/init.d/dbus start
-/etc/init.d/dnsmasq start
-
-# setup handlers
-trap term_handler SIGTERM
-trap term_handler SIGKILL
-
-/etc/init.d/hostapd start
 
 sleep infinity &
 child=$!
