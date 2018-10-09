dd391fa29f554f53e7aa1c1de9e70ac68aae0496
Arjan van Schoonhoven <arjan.vanschoonhoven@ultimatesoftware.nl>
2018-10-09 14:39:57 +0200

    hostapd2 changes

--- Docker/hostapd2/entrypoint.sh
+++ Docker/hostapd2/entrypoint.sh
@@ -5,7 +5,8 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
 
-dnsmasq wlan0
+echo "Starting DNSMASQ daemon ..."
+/usr/sbin/dnsmasq
 # Capture external docker signals
 trap 'true' SIGINT
 trap 'true' SIGTERM
