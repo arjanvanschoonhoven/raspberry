9a467e7efb9fcb1cc391e0256f5905512bb73e30
Arjan van Schoonhoven <arjan.vanschoonhoven@ultimatesoftware.nl>
2018-10-09 14:25:15 +0200

    entrypoint change

--- Docker/hostapd2/entrypoint.sh
+++ Docker/hostapd2/entrypoint.sh
@@ -5,12 +5,14 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
 
-service dnsmasq start
-service hostapd start
- 
-rc-update add iptables
-rc-update add dnsmasq
-rc-update add hostapd
+dnsmasq wlan0
+# Capture external docker signals
+trap 'true' SIGINT
+trap 'true' SIGTERM
+trap 'true' SIGHUP
+
+echo "Starting HostAP daemon ..."
+/usr/sbin/hostapd /etc/hostapd/hostapd.conf
 
 sleep infinity &
 child=$!
