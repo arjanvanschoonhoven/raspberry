712a385bd938d5ce617f56346cb4aaf1faf547fb
Arjan van Schoonhoven <arjan.vanschoonhoven@ultimatesoftware.nl>
2018-10-09 14:08:04 +0200

    Typo fixed

--- Docker/hostapd2/entrypoint.sh
+++ Docker/hostapd2/entrypoint.sh
@@ -5,7 +5,7 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
 
-/etc/init.d/dnsmaq start
+/etc/init.d/dnsmasq start
 /etc/init.d/hostapd start
 
 sleep infinity &
